1d8e7297c98b83e23e5f10de439410f23eb2a6f9
Harish <harishsmdm2003@gmail.com>
2026-08-03 11:45:06 +0530

    Fixed component error

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,23 @@ import { useState } from 'react'
 import Home from './pages/Home'
 import { Route } from 'react-router-dom';
 
-import { useState } from "react";
+
 
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Register from "./pages/Register";
-import { Route, Routes } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import Login from "./pages/Login";
 
 function App() {
   return (
     <>
-   
+
 
 
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
